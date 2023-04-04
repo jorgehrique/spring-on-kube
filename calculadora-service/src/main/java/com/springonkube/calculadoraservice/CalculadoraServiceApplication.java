15f7dabf185795a92edcdcbed93a003334b4c847
Jorge Henrique <jorgehasd@gmail.com>
2023-04-04 19:13:07 -0300

    create calculadora service project

---
+++ calculadora-service/src/main/java/com/springonkube/calculadoraservice/CalculadoraServiceApplication.java
@@ -0,0 +1,13 @@
+package com.springonkube.calculadoraservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraServiceApplication.class, args);
+	}
+
+}
